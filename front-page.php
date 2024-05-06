6f4a956b08ac21dcbad3b691369de638d4655e7f
Rodrigo Vieira Eufrasio da Silva <rodrigo.vesilva@sp.senac.br>
2024-05-06 15:53:38 -0300

    update in frontPage

--- front-page.php
+++ front-page.php
@@ -5,8 +5,4 @@
 <main>
 
 </main>
-
-<footer>
-    <small>Desenvolvido por: <abbr title="Gerência de Comunicação e Relações Institucionais">GCR</abbr> - TBI - Copyright <?php date('Y')?> - Senac São Paulo</small>
-</footer>
 <?php get_footer();?>
